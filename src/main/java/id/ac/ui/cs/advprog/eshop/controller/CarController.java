441cef3ece18c506905d1c24fb254dfd2c3cd6a9
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-03-03 15:00:42 +0700

    refactor: delete implements so CarController stand by itself

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/car")
 @RequiredArgsConstructor
-class CarController extends ProductController {
+class CarController {
     private final CarService carservice;
     private static final String REDIRECT_CARLIST = "redirect:listCar";
 
