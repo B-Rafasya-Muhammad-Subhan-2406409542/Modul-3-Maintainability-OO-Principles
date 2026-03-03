6a5bd29efc1b5714e17eb26931d76fb73972ec96
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-03-03 11:43:18 +0700

    refactor: capitalize view names and use constants in CarController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -4,6 +4,7 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,81 +14,85 @@ import java.util.List;
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
     @Autowired
     private ProductService service;
+    private static final String REDIRECT_LIST = "redirect:list";
 
     @GetMapping("/create")
     public String createProductPage(Model model){
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
     public String createProductPost(@ModelAttribute Product product, Model model){
         service.create(product);
-        return "redirect:list";
+        return REDIRECT_LIST;
     }
 
     @GetMapping("/list")
     public String productListPage(Model model){
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 
     @GetMapping("/edit/{productId}")
     public String editProductPage(@PathVariable String productId, Model model){
         Product product = service.findById(productId);
         model.addAttribute("product", product);
-        return "editProduct";
+        return "EditProduct";
     }
 
     @PostMapping("/edit")
     public String editProductPost(@ModelAttribute Product product, Model model){
         System.out.println(product.getProductId());
         service.update(product.getProductId(), product);
-        return "redirect:list";
+        return REDIRECT_LIST;
     }
 
     @PostMapping("/delete")
     public String deleteProduct(@RequestParam("productId")String productId){
         service.deleteById(productId);
-        return "redirect:list";
+        return REDIRECT_LIST;
     }
 }
 
 @Controller
 @RequestMapping("/car")
 class CarController extends ProductController {
+
     @Autowired
     private CarServiceImpl carservice;
+    private static final String REDIRECT_CARLIST = "redirect:listcar";
 
     @GetMapping("/createCar")
     public String createCarPage(Model model) {
         Car car = new Car();
         model.addAttribute("car", car);
-        return "createCar";
+        return "CreateCar";
     }
 
     @PostMapping("/createCar")
     public String createCarPost(@ModelAttribute Car car, Model model) {
         carservice.create(car);
-        return "redirect:listCar";
+        return REDIRECT_CARLIST;
     }
 
     @GetMapping("/listCar")
     public String carListPage(Model model) {
         List<Car> allCars = carservice.findAll();
         model.addAttribute("cars", allCars);
-        return "carList";
+        return "CarList";
     }
 
     @GetMapping("/editCar/{carId}")
     public String editCarPage(@PathVariable String carId, Model model) {
         Car car = carservice.findById(carId);
         model.addAttribute("car", car);
-        return "editCar";
+        return "EditCar";
     }
 
     @PostMapping("/editCar")
@@ -95,12 +100,12 @@ class CarController extends ProductController {
         System.out.println(car.getCarId());
         carservice.update(car.getCarId(), car);
 
-        return "redirect:listCar";
+        return REDIRECT_CARLIST;
     }
 
     @PostMapping("/deleteCar")
     public String deleteCar(@RequestParam("carId") String carId) {
         carservice.deleteCarById(carId);
-        return "redirect:listCar";
+        return REDIRECT_CARLIST;
     }
 }
